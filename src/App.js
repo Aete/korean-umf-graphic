b61380bba08068838bda7a0aff1772f6a6389487
Seunggyun <s.han@aresa.io>
2023-07-10 17:49:59 +0900

    :tada: create a template (-ing)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import Container from './graphcis/Container';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  body {
+    background-color:blue;
+  }
+`;
+
 function App() {
-  return <div className='App'>test</div>;
+  return (
+    <div className='App'>
+      <GlobalStyles />
+      <Container />
+    </div>
+  );
 }
 
 export default App;
